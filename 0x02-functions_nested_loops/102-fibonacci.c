eeccb502ed75bc19a04e92b0abff7681afe36775
Peter Oyedeji <electropeter98@gmail.com>
2022-10-18 18:04:36 -0700

    C functions, nested loop - Advanced tasks push

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 49; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		printf("%ld, ", fibonacci[i]);
+	}
+	fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+	printf("%ld\n", fibonacci[i]);
+	return (0);
+}
